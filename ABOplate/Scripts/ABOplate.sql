178667ab9190b5f6b1fddcacdc832a94fd91909b
ldh0115 <66412443+ldh0115@users.noreply.github.com>
2020-06-14 20:04:55 +0900

    Update ABOplate.sql

--- ABOplate/Scripts/ABOplate.sql
+++ ABOplate/Scripts/ABOplate.sql
@@ -1,1 +1,79 @@
-SELECT * FROM MEMBER;
+DROP TABLE MEMBER;
+
+CREATE TABLE "MEMBER"(
+	member_id VARCHAR2(30),
+	member_name VARCHAR2(30),
+	member_email VARCHAR2(30),
+	member_nickname VARCHAR2(50) UNIQUE,
+	member_stamp NUMBER,
+	member_preference_food VARCHAR2(100),
+	member_region VARCHAR2(20),
+	member_age_group NUMBER,
+	member_report NUMBER,
+	member_last_report DATE,
+	number_type NUMBER,
+	CONSTRAINT NUMBER_PK PRIMARY KEY(member_id)
+);
+
+DROP TABLE "MEMBER";
+
+CREATE SEQUENCE restaurant_seq;
+DROP SEQUENCE restaurant_seq;
+
+CREATE TABLE RESTAURANT(
+	restaurant_num NUMBER,
+	restaurant_tel VARCHAR2(30),
+	restaurant_name VARCHAR2(30),
+	restaurant_address_road VARCHAR2(200),
+	restaurant_address VARCHAR2(200),
+	restaurant_food_category VARCHAR2(50),
+	restaurant_parking CHAR,
+	restaurant_day_off VARCHAR(30),
+	restaurant_menu_board VARCHAR2(500),
+	restaurant_local_currency CHAR,
+	restaurant_age_group NUMBER,
+	restaurant_business_day VARCHAR2(1000),
+	restaurant_ration_total NUMBER DEFAULT 0,
+	restaurant_hit NUMBER DEFAULT 0,
+	CONSTRAINT RESTAURANT_PK PRIMARY KEY(restaurant_num),
+	CONSTRAINT RESTAURANT_PARKING CHECK(restaurant_parking = 'O' OR restaurant_parking = 'X'),
+	CONSTRAINT RESTAURANT_LOCAL_CURRENCY CHECK(restaurant_local_currency = 'O' OR restaurant_local_currency = 'X')	
+);
+
+DROP TABLE RESTAURANT;
+
+CREATE SEQUENCE review_seq;
+DROP SEQUENCE review_seq;
+
+CREATE TABLE REVIEW(
+	review_num NUMBER,
+	restaurant_num NUMBER,
+	member_nickname VARCHAR2(50),
+	review VARCHAR2(3000),
+	review_ration NUMBER,
+	review_report CHAR,
+	review_upload_date DATE,
+	CONSTRAINT REVIEW_PK PRIMARY KEY(review_num),
+	CONSTRAINT REVIEW_FK_1 FOREIGN KEY(restaurant_num) REFERENCES RESTAURANT(restaurant_num),
+	CONSTRAINT REVIEW_FK_2 FOREIGN KEY(member_nickname) REFERENCES "MEMBER"(member_nickname)
+);
+
+CREATE SEQUENCE bookmark_seq;
+DROP SEQUENCE bookmark_seq;
+
+CREATE TABLE BOOKMARK(
+	bookmark_num NUMBER,
+	email VARCHAR2(50),
+	restaurant_num NUMBER,
+	CONSTRAINT BOOKMARK_FK_1 FOREIGN KEY(email) REFERENCES "MEMBER"(member_email),
+	CONSTRAINT BOOKMARK_FK_2 FOREIGN KEY(restaurant_num) REFERENCES RESTAURANT(restaurant_num),	
+)
+
+CREATE TABLE MENU_LIST(
+	restaurant_num NUMBER,
+	menu VARCHAR2(30),
+	price VARCHAR2(30),
+	CONSTRAINT MENU_LIST_FK FOREIGN KEY(restaurant_num) REFERENCES RESTAURANT(restaurant_num)
+);
+
+DROP TABLE MENU_LIST;
