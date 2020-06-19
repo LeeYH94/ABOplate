b748f267840a7b916e7d15b4826a710913bf1509
iij0573 <51044868+iij0573@users.noreply.github.com>
2020-06-19 20:01:30 +0900

    commit 1

--- ABOplate/src/com/aboplate/app/restaurant/MemberRecRestaruantAction.java
+++ ABOplate/src/com/aboplate/app/restaurant/MemberRecRestaruantAction.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.aboplate.action.Action;
 import com.aboplate.action.ActionForward;
@@ -12,31 +13,30 @@ import com.aboplate.app.member.dao.MemberBean;
 import com.aboplate.app.restaurant.dao.RestaurantBean;
 import com.aboplate.app.restaurant.dao.RestaurantDAO;
 
-public class MemberRecRestaruant implements Action{
+public class MemberRecRestaruantAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
-		
+
 		RestaurantDAO rDao = new RestaurantDAO();
 		MemberBean mBean = new MemberBean();
 		ActionForward forward = new ActionForward();
-		
-		String food = request.getParameter("member_preference_food");
-		int age_group = Integer.parseInt(request.getParameter("member_age_group"));
-		String region = request.getParameter("member_region");
-		List<MemberBean> memberBeanList = rDao.getBestMemberRestaurantList(food,age_group,region);
-		
-		if(rDao.memberRecommend(food, age_group, region)) {
-			//추천리스트목록 띄워주기
-			if(memberBeanList!=null) {
-				request.setAttribute("memberBeanList", memberBeanList);
-			}
-		
+		HttpSession session = request.getSession();
+
+		String session_id = (String) session.getAttribute("session_id");
+
+		List<RestaurantBean> restaruantBeanList = rDao.getMemberChoiceList(session_id);
+
+		if (restaruantBeanList != null) {
+			request.setAttribute("restaruantBeanList", restaruantBeanList);
+
 			forward.setRedirect(false);
-			//맛집 추천후 이동할 페이지
+			forward.setPath("/app/member/main2.jsp");
 			return forward;
+
 		}
 		return null;
+
 	}
 }
