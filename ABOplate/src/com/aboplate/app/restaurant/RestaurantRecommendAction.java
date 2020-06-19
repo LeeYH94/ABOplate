1accc8ab349f8640be377b15fae1c51f2e753a18
smy1550 <mk_1550@naver.com>
2020-06-19 23:47:12 +0900

    restaurant recommend action 수정
    수정1

--- ABOplate/src/com/aboplate/app/restaurant/RestaurantRecommendAction.java
+++ ABOplate/src/com/aboplate/app/restaurant/RestaurantRecommendAction.java
@@ -15,32 +15,31 @@ public class RestaurantRecommendAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-		
 		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
 		RestaurantDAO r_dao = new RestaurantDAO();
-		RestaurantBean r_bean = new RestaurantBean();
+		RestaurantBean r_bean;
 		
-		//r_dao���� �޼ҵ�� �������� 
-		r_bean = r_dao.getPopularList(restaurant_num);
 		//�����ؾ���
-		List<RestaurantBean> restaurantBeanList = r_dao.getPopularList(restaurant_num);
+		List<RestaurantBean> restaurantBeanList = r_dao.getPopularList();
+		List<RestaurantBean> popularList = new List<>();
+		int popularListLength = restaurantBeanList.size();
 		
-		if(r_bean !=null) {
-			request.setAttribute("restaurantBeanList", restaurantBeanList);
-			if( restaurantBeanList!=null) {
-				request.setAttribute(" restaurantBeanList", restaurantBeanList);
+		
+		double randomValue = Math.random()*popularListLength;
+		while(popularList.size()<8) {
+			r_bean = restaurantBeanList.get((int)randomValue);
+			if(popularList.contains(r_bean)) {
+				continue;
+			}else {
+				popularList.add(r_bean);
 			}
-			ActionForward forward = new ActionForward();
-			forward.setRedirect(true);
-			forward.setPath("/main2.jsp");
-			return forward;
 		}
-		double randomValue = Math.random();
-		int ran = (int)(randomValue * 8)-1;
-		RestaurantBean getValue = restaurantBeanList.get(ran);
-		restaurantBeanList.remove(ran);
+		
+		
+	
+		
 		
 		return null;
 	}
-
 }
