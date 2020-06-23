cf51800f558ed11a558a8e6408214bf1a53b795b
LeeYH94 <55489961+LeeYH94@users.noreply.github.com>
2020-06-23 14:32:56 +0900

    review recommend action

--- ABOplate/src/com/aboplate/app/restaurant/RestaurantReviewRecommendAction.java
+++ ABOplate/src/com/aboplate/app/restaurant/RestaurantReviewRecommendAction.java
@@ -8,10 +8,10 @@ import javax.servlet.http.HttpSession;
 
 import com.aboplate.action.Action;
 import com.aboplate.action.ActionForward;
-import com.aboplate.app.restaurant.dao.RestaurantBean;
 import com.aboplate.app.restaurant.dao.RestaurantDAO;
+import com.aboplate.app.restaurant.dao.ReviewBean;
 
-public class RestaurantBookmarkOkAction implements Action {
+public class RestaurantReviewRecommendAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -21,20 +21,16 @@ public class RestaurantBookmarkOkAction implements Action {
 		HttpSession session = request.getSession();
 		RestaurantDAO restaurantDao = new RestaurantDAO();
 		
-		String id = (String) session.getAttribute("sessionId");
+		String id = session.getAttribute("sessionId").toString();
+		int reviewNum = (int) request.getAttribute("reviewNum");
 		
-		//레스토랑 번호 가져옴
-		RestaurantBean restaurantBean = (RestaurantBean) request.getAttribute("restaurantBean");
-		int seq = restaurantBean.getRestaurant_num();
-		
-		//boolean값으로, DB에 있으면 빼기, DB에 없으면 추가
 		PrintWriter out = response.getWriter();
 		
-		if(restaurantDao.checkBookmark(id, seq)) {
-			restaurantDao.popBookmark(id, seq);
+		if(restaurantDao.checkReviewRecommend(id, reviewNum)) {
+			restaurantDao.popReviewRecommend(id, reviewNum);
 			out.print("pop");
 		} else {
-			restaurantDao.addBookmark(id, seq);
+			restaurantDao.addReviewRecommend(id, reviewNum);
 			out.print("add");
 		}
 		
