d173215ba807ce8ff6e894cb63326d36ad3694e6
LeeYH94 <55489961+LeeYH94@users.noreply.github.com>
2020-06-23 14:34:54 +0900

    Merge branch 'master' of https://github.com/LeeYH94/ABOplate

--- ABOplate/src/com/aboplate/app/restaurant/RestaurantBookmarkOkAction.java
+++ ABOplate/src/com/aboplate/app/restaurant/RestaurantBookmarkOkAction.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 
 import com.aboplate.action.Action;
 import com.aboplate.action.ActionForward;
+import com.aboplate.app.bookmark.dao.BookmarkDAO;
 import com.aboplate.app.restaurant.dao.RestaurantBean;
 import com.aboplate.app.restaurant.dao.RestaurantDAO;
 
@@ -19,25 +20,24 @@ public class RestaurantBookmarkOkAction implements Action {
 		response.setCharacterEncoding("UTF-8");
 		
 		HttpSession session = request.getSession();
-		RestaurantDAO restaurantDao = new RestaurantDAO();
-		
+		BookmarkDAO bookmarkDao = new BookmarkDAO();
+
 		String id = (String) session.getAttribute("sessionId");
-		
+
 		//레스토랑 번호 가져옴
 		RestaurantBean restaurantBean = (RestaurantBean) request.getAttribute("restaurantBean");
-		int seq = restaurantBean.getRestaurant_num();
-		
+		int restaurantNum = restaurantBean.getRestaurant_num();
+
 		//boolean값으로, DB에 있으면 빼기, DB에 없으면 추가
 		PrintWriter out = response.getWriter();
-		
-		if(restaurantDao.checkBookmark(id, seq)) {
-			restaurantDao.popBookmark(id, seq);
+		if(bookmarkDao.checkBookmark(id, restaurantNum) == 1) {
+			bookmarkDao.popBookmark(id, restaurantNum);
 			out.print("pop");
 		} else {
-			restaurantDao.addBookmark(id, seq);
+			bookmarkDao.addBookmark(id, restaurantNum);
 			out.print("add");
 		}
-		
+
 		return null;
 	}
 
