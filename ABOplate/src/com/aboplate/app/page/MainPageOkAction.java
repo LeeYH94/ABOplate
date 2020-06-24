b111e3d1d9c020354efe8b439344c4b54f7cc461
LeeYH94 <55489961+LeeYH94@users.noreply.github.com>
2020-06-24 21:08:37 +0900

    mainPage updated
    메인페이지에 추천과 인기맛집도 넣어야 해서 레스토랑에 있는 것들을 옮겼습니다

---
+++ ABOplate/src/com/aboplate/app/page/MainPageOkAction.java
@@ -0,0 +1,56 @@
+package com.aboplate.app.page;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.aboplate.action.Action;
+import com.aboplate.action.ActionForward;
+import com.aboplate.app.member.dao.MemberBean;
+import com.aboplate.app.restaurant.dao.RestaurantBean;
+import com.aboplate.app.restaurant.dao.RestaurantDAO;
+
+public class MainPageOkAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		ActionForward forward = new ActionForward();
+		RestaurantDAO restaurantDao = new RestaurantDAO();
+		HttpSession session = request.getSession();
+		RestaurantBean restaurantBean;
+		
+		String id = session.getAttribute("sessionId").toString();
+		
+		List<RestaurantBean> tempPopularList = restaurantDao.getPopularList();
+		List<RestaurantBean> popularList = new ArrayList<>();
+		int tempPopularListLength = tempPopularList.size();
+		
+		while(popularList.size() < 8) {
+			restaurantBean = tempPopularList.get((int)(Math.random()*tempPopularListLength));
+			if(popularList.contains(restaurantBean)) {
+				continue;
+			} else {
+				popularList.add(restaurantBean);
+			}
+		}
+		
+		List<RestaurantBean> recommendList = new ArrayList<>();
+		if(id != null) {
+			recommendList = restaurantDao.getMemberChoiceList(id);
+			request.setAttribute("recommendList", recommendList);
+		}
+		
+		request.setAttribute("popularList", popularList);
+		
+		forward.setPath("/main.jsp");
+		forward.setRedirect(false);
+		
+		return forward;
+	}
+}
