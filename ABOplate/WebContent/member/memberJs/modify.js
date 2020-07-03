dbcf416e5a9c78556c03742b360b84720144ac80
smy1550 <mk_1550@naver.com>
2020-07-04 00:12:22 +0900

    commit
    commit

--- ABOplate/WebContent/member/memberJs/modify.js
+++ ABOplate/WebContent/member/memberJs/modify.js
@@ -4,11 +4,7 @@ var nicknameCheck = false;
 function modifySubmit() {
   	var form = document.modifyForm;
   			
- 	 if ('' == form.memberPassword.value) {
-		alert("현재 비밀번호를 입력해주세요");
-		form.memberPassword.focus();
-		return false;
-	} else if ('' == form.newMemberPassword.value) {
+ 	 if ('' == form.newMemberPassword.value) {
 		alert("새로운 비밀번호 확인을 입력해주세요");
 		form.newMemberPassword.focus();
 		return false;
@@ -16,11 +12,7 @@ function modifySubmit() {
 		form.newMemberRePassword.focus();
 		alert("비밀번호 확인이 일치하지 않습니다");
 		return false;
-	} else if(form.memberPassword.value==form.newMemberPassword.value){
-		alert("현재 비밀번호와 일치합니다. 새로운 비밀번호를 입력해주세요");
-		form.newMemberPassword.focus();
-		return false;
-	}	
+	} 
 	else if ('' == form.memberFavorite.value) {
 		
 		alert("선호음식을 입력해주세요");
