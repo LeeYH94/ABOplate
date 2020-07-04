349395d2f30cbc8fceae28358cd4b4b18ecc1b6c
LeeYH94 <55489961+LeeYH94@users.noreply.github.com>
2020-07-05 00:13:51 +0900

    updated stamp
    memberBean session에 저장

--- ABOplate/src/com/aboplate/app/restaurant/RestaurantViewAction.java
+++ ABOplate/src/com/aboplate/app/restaurant/RestaurantViewAction.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpSession;
 import com.aboplate.action.Action;
 import com.aboplate.action.ActionForward;
 import com.aboplate.app.bookmark.dao.BookmarkDAO;
+import com.aboplate.app.member.dao.MemberDAO;
 import com.aboplate.app.picture.dao.PictureBean;
 import com.aboplate.app.picture.dao.PictureDAO;
 import com.aboplate.app.restaurant.dao.RestaurantBean;
@@ -61,10 +62,11 @@ public class RestaurantViewAction implements Action{
 		request.setAttribute("currentPage", page);
 		request.setAttribute("startPage", startPage);
 		request.setAttribute("endPage", endPage);
-
+		
 		LinkedHashMap<ReviewBean, Object> reviewMap = new LinkedHashMap<>();
 		if(session.getAttribute("sessionId") != null) {
 			String id = session.getAttribute("sessionId").toString();
+			session.setAttribute("memberBean", new MemberDAO().getMemberInfo(id));
 			request.setAttribute("bookmarkCheck", bookmarkDao.checkBookmark(id, restaurantNum));
 			
 			for (ReviewBean reviewBean : reviewDao.getReviewList(startRow, endRow, restaurantNum)) {
