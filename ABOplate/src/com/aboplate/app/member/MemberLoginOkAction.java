54f4b9eca84f1b0e0f4f70271444a6efedca7a85
LeeYH94 <55489961+LeeYH94@users.noreply.github.com>
2020-07-05 02:13:17 +0900

    Update MemberLoginOkAction.java
    로그인 시 main.jsp로 가는 경로 수정

--- ABOplate/src/com/aboplate/app/member/MemberLoginOkAction.java
+++ ABOplate/src/com/aboplate/app/member/MemberLoginOkAction.java
@@ -69,7 +69,7 @@ public class MemberLoginOkAction implements Action {
 		if(mBean != null) {
 			session.setAttribute("memberBean", mBean);
 			session.setAttribute("sessionId", id);
-			forward.setPath(request.getContextPath()+"/main.jsp");	
+			forward.setPath(request.getContextPath()+"/index.jsp");	
 		} else {
 			forward.setPath(request.getContextPath()+"/member/MemberLogin.me?login=false");
 		}
