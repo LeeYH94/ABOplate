6f6e00cc9be08f5f8e073be7e9257b9157fa4dc4
bronzeq <36151742+bronzeq@users.noreply.github.com>
2020-07-05 20:17:09 +0900

    cookie

--- ABOplate/WebContent/saveCookie.js
+++ ABOplate/WebContent/saveCookie.js
@@ -1,32 +1,31 @@
 /**
+ * iteam pg
+ * 
+ * cookie[]cookies=request.getCookies(); Cookie cookie=null;
  * 
  */
-
-
-function CookieSave() {
-	
-	var rBean= RestaurantBean;
-	var items = getCookie('rBean');
-	
-	var maxItemNum = 5; // 최대 저장 가능한 아이템개수
-	  var expire = 7; // 쿠키값을 저장할 기간
-	  
-	  if (items) {
-	    var itemArray = items.split(',');
-	    if (itemArray.indexOf(id) != -1) {
-	      // 이미 존재하는 경우 종료
-	      console.log('Already exists.');
-	    }
-	    else {
-	      // 새로운 값 저장 및 최대 개수 유지하기
-	      itemArray.unshift(id);
-	      if (itemArray.length > maxItemNum ) itemArray.length = 5;
-	      items = itemArray.join(',');
-	      setCookie('productItems', items, expire);
-	    }
-	  }
-	  else {
-	    // 신규 id값 저장하기
-	    setCookie('productItems', id, expire);
-	  }
-	}
+function printSaveCookie() {
+	document.Cookie="cookie=menu";	  
+	  //var items = getCookie('rBean'); var cookieList={};
+	  var split=document.cookie.split(','); 
+	  var name_value = split[i].split("=");	 
+	  var maxItemNum = 5; 
+ 
+	 if(document.cookie&&dicument.cookie !=''){ 
+		 for (var i = 0; i < split.length; i++) { name_value[0]=name_value[0].replace('/^/.');
+	 cookieList[decodeURIComponent(name_value[0])] =decodeURIComponent(name_value[1]); } } return cookieList; }
+	 
+	 
+		  if (items) { var itemArray = items.split(',');
+		  if(itemArray.indexOf(id) != -1) { // 이미 존재하는 경우 종료
+		console.log('Already exists.');
+		} else { // 새로운 값 저장 및 최대 개수 유지하기
+			itemArray.unshift(id);
+			
+		  if (itemArray.length > maxItemNum ) 
+			  {
+			  items = itemArray.join(',');
+			  setCookie('productItems', items, expire);
+		 } else {  setCookie('productItems', id, expire); } // 신규 id값 저장하기
+		  }
+		  }
